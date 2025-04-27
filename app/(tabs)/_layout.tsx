2505e5f6cc9000f601943f601e1429065718898e
Isaac Blanco <nuclearhands2001@gmail.com>
2025-04-27 01:49:15 -0500

    add: creating the profile page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -36,10 +36,17 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'Explores',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({color}) => <IconSymbol size={28} name="person.fill" color={color}></IconSymbol>
+        }}
+      ></Tabs.Screen>
     </Tabs>
   );
 }
